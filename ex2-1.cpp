52ab24cd3d5438bd8b2ca84f7b146df03cfaba63
peter-peng1104 <109029@mjes.tp.edu.tw>
2024-10-24 20:02:54 +0800

    Update ex2-1.cpp

--- ex2-1.cpp
+++ ex2-1.cpp
@@ -2,7 +2,10 @@
 
 int main()
 {
-      printf("Hello world");
       
+      printf("1:Hello world!\n");
+      printf("2:This sentence will work!\n");
+      printf("3:I love you!\n");
+      printf("4:I like you!\n");
       return 0;
 }
