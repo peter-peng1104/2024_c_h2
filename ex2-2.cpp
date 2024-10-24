210be85bddc786a3f6d1b2cf4e3240b060a851d8
peter-peng1104 <109029@mjes.tp.edu.tw>
2024-10-24 20:45:07 +0800

    Update ex2-2.cpp

--- ex2-2.cpp
+++ ex2-2.cpp
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main()
+{
+      
+      printf("1:Hello world!\n3:I love you!\n\t4:I like you!\n");
+      printf("2:This sentence will work!\n");
+      printf("3:I love you!\n3:\\I love you!\n3:\"I love you!\n");
+      printf("4:I like you!\n");
+      return 0;
+}
