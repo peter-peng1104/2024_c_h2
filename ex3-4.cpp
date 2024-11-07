e492949b8eefce4e44200c58a4e2f63fa699c7fc
peter-peng1104 <109029@mjes.tp.edu.tw>
2024-11-07 20:52:35 +0800

    Create ex3-4.cpp

---
+++ ex3-4.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  int x,y;
+  
+    printf("please input int x=");
+    scanf("%d",&x);
+    printf("please input int y=");
+    scanf("%d",&y);
+    printf("x+y= %d", x+y);// x-y,x*y ,x/y
+    return 0;
+
+}
