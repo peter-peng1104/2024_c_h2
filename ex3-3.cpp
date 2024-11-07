05fa810d04bec83c3f1cb63e4a819cff8448f3c4
peter-peng1104 <109029@mjes.tp.edu.tw>
2024-11-07 20:22:02 +0800

    Create ex3-3.cpp

---
+++ ex3-3.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  char a = 65, b = 66, c = 69;
+  //char a = 'a', b = 'b', c = 'c';
+printf("a = %c\n",a);
+printf("b = %c\n",b); 
+printf("c = %c\n",c);
+    return 0;
+}
