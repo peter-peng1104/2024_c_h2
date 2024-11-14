0f6411a93c05276364a5f07dcd686fdddac910a7
peter-peng1104 <109029@mjes.tp.edu.tw>
2024-11-14 19:56:42 +0800

    Create ex4-1.cpp

---
+++ ex4-1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  int x,y;
+  
+    printf("please input int x=");
+    scanf("%d",&x);
+    printf("please input int y=");
+    scanf("%d",&y);
+    printf("x+y= %d\n", x+y); // x-y ,x*y ,x/y
+   printf("x-y= %d\n", x-y);
+    printf("x*y= %d\n", x*y); 
+    printf("x/y= %d\n", x/y);
+    printf("x mod y= %d\n",x%y);
+    printf("x>10 && y < 5 %d\n",(x>10 && y < 5));
+   
+    return 0;
+}
