5957dad88b590100c8ad1d99d2ac71eed459889c
peter-peng1104 <109029@mjes.tp.edu.tw>
2024-11-21 20:24:24 +0800

    Create h5-1.cpp

---
+++ h5-1.cpp
@@ -0,0 +1,18 @@
+/*
+1.輸入2個整數(x,y)，求兩數的平方(x*x+y*y)和並輸出
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int x,y;
+    
+    printf("please input int x=");
+    scanf("%d",&x);
+    printf("please input int y=");
+    scanf("%d",&y);
+    printf("x+y= %d\n",x+y); // x-y,x*y,x/y
+
+    return 0;
+}
