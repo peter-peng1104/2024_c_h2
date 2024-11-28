3a42067ce50533bc1ed4befad3394d9e54b0c1f4
peter-peng1104 <109029@mjes.tp.edu.tw>
2024-11-28 19:53:52 +0800

    Create ex6-2.cpp

---
+++ ex6-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    int a[5];
+    int i;
+    for(i=0;i<5;i++){
+       // a[i]=i;
+       printf("input int:");
+       scanf("%d",&a[i]);
+    }
+    for(i=0;i<5;i++){
+    printf("a[%d)=%d ",i,a[i]);
+
+    }
+    return 0;
+}
